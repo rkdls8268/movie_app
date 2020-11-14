f7f00f2a351ec7c9ff0754623de3b95dec08948c
rkdls8268 <rkdls8268@gmail.com>
2020-11-14 16:05:08 +0900

    dynamic component generation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from "react";
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>;
+function Food({name, picture}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={picture} />
+    </div>
+  );
 }
 
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "https://user-images.githubusercontent.com/31029088/99141402-66fcff00-268e-11eb-9617-995794134a95.png"
+  },
+  {
+    name: "ramen",
+    image: "https://user-images.githubusercontent.com/31029088/99141415-81cf7380-268e-11eb-84cb-5ccc0a9fd75a.jpeg"
+  },
+  {
+    name: "salad",
+    image: "https://user-images.githubusercontent.com/31029088/99141424-8f84f900-268e-11eb-89b6-6010b70574ad.png"
+  },
+  {
+    name: "chicken",
+    image: "https://user-images.githubusercontent.com/31029088/99141438-a9264080-268e-11eb-955c-d950d6ab62e6.jpg"
+  },
+];
+
 function App() {
-  return <div> Hello! 
-          <Food fav="Kimchi" />
-          <Food fav="Ramen" />
-          <Food fav="salad" />
-          <Food fav="chicken" />
-        </div>;
+  return (
+      <div>
+        {foodILike.map(food => <Food name={food.name} picture={food.image}/>)}
+      </div>
+  )
 }
 
 export default App;
