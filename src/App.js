70c6e423c651a2aae40f55dd3688474be9f74e15
rkdls8268 <rkdls8268@gmail.com>
2020-11-18 16:01:36 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 class App extends React.Component{
+  // constructors는 js를 알면 됨.
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0
   };
@@ -13,7 +18,22 @@ class App extends React.Component{
     this.setState(current => ({count: current.count - 1}));
   };
 
+  componentDidMount() {
+    console.log("component rendered")
+  }
+
+  componentDidUpdate() {
+    // setState를 호출하면, component를 호출하고 먼저 render를 호출한 다음 업데이트가 완료되었다고 말하면 componentDidUpdate가 실행된다. 
+    console.log("I just update!");
+  }
+
+  componentWillUnmount() {
+    // component가 떠날 때 호출된다. 하지만 브라우저 상에서는 알 길이 없네..ㅎ
+    console.log("Goodbye");
+  }
+
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
