e91a595cc157c6178c378ab435d97b59577cd300
rkdls8268 <rkdls8268@gmail.com>
2020-11-20 16:10:10 +0900

    rendering the movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./movies";
 
 class App extends React.Component {
   state = {
@@ -9,7 +10,8 @@ class App extends React.Component {
   
   // async await 을 하는 것은 기본적으로 js에게 getMovies()가 조금 시간이 필요하고 우리는 그걸 기다려야 한다는 것을 명시한다. 
   getMovies = async() => {
-    const movies = axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
   }
 
   // 이론적으로 우리가 할 일은 componentDidMount에서 data를 fetch하는 것이다.
@@ -18,8 +20,10 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{this.state.isLoading ? "Loading..." : "we are ready"}</div>
+    const { isLoading, movies } = this.state;
+    return <div>{this.state.isLoading ? "Loading..." : movies.map(movie => (
+      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+    ))}</div>
   }
 }
 
