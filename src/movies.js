94a03a951c861d00f7ce4ce3328f1f065e742efe
rkdls8268 <rkdls8268@gmail.com>
2020-11-21 22:14:49 +0900

    adding array(genres)

--- src/movies.js
+++ src/movies.js
@@ -3,14 +3,17 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 
-function Movie({year, title, summary, poster}) {
+function Movie({year, title, summary, poster, genres}) {
     return (
-    <div class="movies_movie">
+    <div className="movies_movie">
         <img src={poster} alt={title} title={title} />
-        <div class="movie_data">
-            <h3 class="movie_title">{title}</h3>
-            <h5 class="movie_year">{year}</h5>
-            <p class="movie_summary">{summary}</p>
+        <div className="movie_data">
+            <h3 className="movie_title">{title}</h3>
+            <h5 className="movie_year">{year}</h5>
+            <ul className="genres">
+                {genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}
+            </ul>
+            <p className="movie_summary">{summary}</p>
         </div>
     </div>)
 }
@@ -20,7 +23,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
