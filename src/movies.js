514e668190f7c422d8bbb74051c29f1362931c62
rkdls8268 <rkdls8268@gmail.com>
2020-12-12 22:33:49 +0900

    add css

--- src/movies.js
+++ src/movies.js
@@ -10,7 +10,7 @@ function Movie({year, title, summary, poster, genres}) {
         <div className="movie_data">
             <h3 className="movie_title">{title}</h3>
             <h5 className="movie_year">{year}</h5>
-            <ul className="genres">
+            <ul className="movie_genres">
                 {genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}
             </ul>
             {summary.length <= 150 ? 
