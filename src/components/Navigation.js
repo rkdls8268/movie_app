469306cee740874e44b317d19398f7dabb720287
rkdls8268 <rkdls8268@gmail.com>
2020-12-17 18:47:21 +0900

    fix: flex 문제 해결

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import "./Navigation.css"
 
 function Navigation() {
-    return <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+    return <div className="nav">
+        <ul className="nav-ul">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+        </ul>
     </div>
 }
 
--- src/components/movies.js
+++ src/components/movies.js
@@ -6,32 +6,33 @@ import "./Movie.css";
 
 function Movie({id, year, title, summary, poster, genres}) {
     return (
-        <Link to={{
-            pathname: `/movie/${id}`,
-            // movie를 클릭하면 다음의 정보를 movie-detail로 이동하면서 같이 보내준다.
-            state: {
-                year,
-                title,
-                summary,
-                poster,
-                genres
-            }
-        }}>
-            <div className="movies_movie">
-                <img src={poster} alt={title} title={title} />
-                <div className="movie_data">
-                    <h3 className="movie_title">{title}</h3>
-                    <h5 className="movie_year">{year}</h5>
-                    <ul className="movie_genres">
-                        {genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}
-                    </ul>
-                    {summary.length <= 150 ? 
-                        <p className="movie_summary">{summary.slice(0, 150)}</p> :
-                        <p className="movie_summary">{summary.slice(0, 150)}...</p> 
-                    }
-                </div>
+        // Link가 div 밖에 있어서 계속 안 먹었던 것..!
+        <div className="movies_movie">
+            <Link to={{
+                pathname: `/movie/${id}`,
+                // movie를 클릭하면 다음의 정보를 movie-detail로 이동하면서 같이 보내준다.
+                state: {
+                    year,
+                    title,
+                    summary,
+                    poster,
+                    genres
+                }
+            }}>
+            <img src={poster} alt={title} title={title} />
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="movie_genres">
+                    {genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}
+                </ul>
+                {summary.length <= 150 ? 
+                    <p className="movie_summary">{summary.slice(0, 150)}</p> :
+                    <p className="movie_summary">{summary.slice(0, 150)}...</p> 
+                }
             </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
 
